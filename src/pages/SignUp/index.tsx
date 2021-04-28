3359cb10290bd9bd819d0096981b19e6195c95c6
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-28 08:11:13 -0300

    Pagina de SignUp

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="nome" placeholder="Nome" icon={FiUser} />
+          <Input name="E-mail" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="login">
+          <div>
+            <FiArrowLeft />
+            Voltar para logon
+          </div>
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
