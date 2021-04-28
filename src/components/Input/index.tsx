b6b9a0568a84341a2535ab9a5541b0ef8e9008eb
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-28 09:03:05 -0300

    Implementando o Unform

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,36 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons/lib';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }: InputProps) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  icon: Icon,
+  ...rest
+}: InputProps) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  // Assim que o componente for mostrado em tela queremos registrá-lo no unform(registerField)
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path:
+        'value' /* Dentro do html, onde estará o valor a ser capturado? Neste caso estará no value */,
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input {...rest} />
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,17 +9,24 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form
+          initialData={{ email: 'jonathancmpc@gmail.com' }}
+          onSubmit={handleSubmit}
+        >
           <h1>Faça seu cadastro</h1>
 
-          <Input name="nome" placeholder="Nome" icon={FiUser} />
-          <Input name="E-mail" placeholder="E-mail" icon={FiMail} />
+          <Input name="name" placeholder="Nome" icon={FiUser} />
+          <Input name="email" type="email" placeholder="E-mail" icon={FiMail} />
           <Input
             name="password"
             type="password"
@@ -26,7 +34,7 @@ const SignUp: React.FC = () => {
             icon={FiLock}
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="login">
           <div>
