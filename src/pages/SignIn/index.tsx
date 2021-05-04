d9aa1bbf6c84cfd6d99ae996affd99e662ef3f62
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-05-04 09:59:43 -0300

    Validaçao de login com Yup e Unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
+import { FormHandles } from '@unform/core';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import { getValidationErrors } from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: unknown) => {
+    try {
+      formRef.current?.setErrors({}); // Zerando os erros antes de validar novamente
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Informe um e-mail válido'),
+        password: Yup.string().required('Informe sua senha'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false /* Retorna todos os erros e não só o primeiro */,
+      });
+    } catch (err) {
+      formRef.current?.setErrors(getValidationErrors(err));
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit} ref={formRef}>
           <h1>Faça seu logon</h1>
 
-          <Input name="E-mail" placeholder="E-mail" icon={FiMail} />
+          <Input name="email" placeholder="E-mail" icon={FiMail} />
           <Input
             name="password"
             type="password"
@@ -26,7 +51,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="login">
           <div>
